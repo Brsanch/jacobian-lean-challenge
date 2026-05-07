98ae68222ff931728c768365f315899dd8a6b422
Bryan Sanchez <bryan@altar.domain>
2026-05-06 22:10:28 -0600

    fix(zz76): try Dimension.Constructions + FiniteDimensional.Lemmas for Basis

--- JacobianChallenge/Manifold/HodgeFiniteDimensional.lean
+++ JacobianChallenge/Manifold/HodgeFiniteDimensional.lean
@@ -3,8 +3,8 @@ Copyright (c) 2026 Bryan Sanchez. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bryan Sanchez
 -/
-import Mathlib.LinearAlgebra.Basis.Basic
-import Mathlib.LinearAlgebra.FiniteDimensional.Defs
+import Mathlib.LinearAlgebra.Dimension.Constructions
+import Mathlib.LinearAlgebra.FiniteDimensional.Lemmas
 import JacobianChallenge.Manifold.HolomorphicOneForm
 
 /-! # Finite-dimensionality of `HolomorphicOneForm X` for compact Riemann surfaces
